2710e917cb7dd793469d2e0bf9fe7f0a8dc25b78
Osztopáni Kristóf <kristofosztopani@gmail.com>
2023-09-22 13:57:24 +0200

    Changed theme to encorporate the logos colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,35 +6,35 @@ module.exports = {
   content: ['./public/*.html'],
   // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: false, // comment this line and uncomment bellow for custom light and dark mode
-    // themes: [
-    //   {
-    //     dark: {
-    //       "primary": "#c0dbf9",
-    //       "secondary": "#51d11b",
-    //       "accent": "#43c694",
-    //       "neutral": "#131221",
-    //       "base-100": "#262c4a",
-    //       "info": "#799bf6",
-    //       "success": "#2cc364",
-    //       "warning": "#f3a744",
-    //       "error": "#ea2e6a",
-    //     },
-    //   },
-    //   {
-    //     light: {
-    //       "primary": "#13f2ee",
-    //       "secondary": "#bbabf2",
-    //       "accent": "#5facbf",
-    //       "neutral": "#3d223f",
-    //       "base-100": "#eeecf3",
-    //       "info": "#83a0f1",
-    //       "success": "#31c975",
-    //       "warning": "#f3b853",
-    //       "error": "#de2b55",
-    //     },
-    //   },
-    // ],
+    // themes: false, // comment this line and uncomment bellow for custom light and dark mode
+    themes: [
+      {
+        dark: {
+          "primary": "#055990",
+          "secondary": "#2b8a67",
+          "accent": "#43c694",
+          "neutral": "#131221",
+          "base-100": "#262c4a",
+          "info": "#799bf6",
+          "success": "#2cc364",
+          "warning": "#f3a744",
+          "error": "#ea2e6a",
+        },
+      },
+      {
+        light: {
+          "primary": "#055990",
+          "secondary": "#2b8a67",
+          "accent": "#5facbf",
+          "neutral": "#3d223f",
+          "base-100": "#eeecf3",
+          "info": "#83a0f1",
+          "success": "#31c975",
+          "warning": "#f3b853",
+          "error": "#de2b55",
+        },
+      },
+    ],
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
