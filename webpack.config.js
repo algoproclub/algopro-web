6292c710f216961342e78b6d1496b9e27bbd0ac9
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2023-09-24 13:51:49 +0200

    Add banner image for social media posts

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -18,6 +19,11 @@ const config = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'public/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from:'content', to:'content' }
+            ],
         }),
     ],
     module: {
